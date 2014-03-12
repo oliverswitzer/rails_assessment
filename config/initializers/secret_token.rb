e1b0d01e83b830b42978f02b8eae6f6cfcd8815e
Oliver Switzer <oliverswitzer@gmail.com>
2014-03-12 19:04:16 -0400

    rails init + flesh out controller, view, models for posts and comments

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsAssessment2::Application.config.secret_key_base = '22619ccc1d075fc7b36b19abf40a16108ee758fdc43e337b224e342eb908074d29826bd7dd8f1203fde251abced59652daea276a3848e76445475a68e94205d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsAssessment2::Application.config.session_store :cookie_store, key: '_rails_assessment2_session'
